4f8397d59662fcb6bdcd111529f4c06034637d44
RealBeboy <93521745+RealBeboy@users.noreply.github.com>
2024-11-27 09:47:04 +0800

    Add files via upload

---
+++ Salvador_activity3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.salvador_activity3;
+
+/**
+ *
+ * @author Beboy
+ */
+public class Salvador_activity3 {
+
+    public static void main(String[] args) {
+       System.out.println("This is a string");
+       int x = 27;
+       System.out.println(x);
+       char x = 'a';
+       
+      System.out.println("A final variable with a value of:" + x + 2)
+    }
+}
