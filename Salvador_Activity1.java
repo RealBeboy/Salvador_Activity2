0a73832062ef4a45fb514d752853d0020095962c
RealBeboy <93521745+RealBeboy@users.noreply.github.com>
2024-12-01 17:53:17 +0800

    Add files via upload

---
+++ Salvador_Activity1.java
@@ -0,0 +1,45 @@
+package Java_Simple_Activity;
+
+public class Salvador_Activity1 {
+
+	public static void main(String[] args) {
+	System.out.println("1. Write a Java program to display the folllowing pattern.");
+		System.out.println("   J    a   V     V  a");
+		System.out.println("   J   a a   V   V  a a");
+		System.out.println("J  J  aaaaa   V V  aaaaa");
+		System.out.println(" JJ  a     a   V  a     a");
+	
+	
+	System.out.println("2. Write a Java program to print a face");
+		System.out.println(" +*****+");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | '-' |");
+		System.out.println(" +-----+");
+	
+		
+		
+	System.out.println("3. Write a Java program to display the following pattern.");
+		System.out.println("   Simple JAVA Application");
+		System.out.println("************************************");
+		System.out.println("Fullname: Justine Roy P. Salvador");
+		System.out.println("************************************");
+		
+		System.out.println();
+		
+		System.out.println("***              *** ***        *** ***        *** ************");
+		System.out.println(" ***            ***  *****      *** ***        *** ************");
+		System.out.println("  ***          ***   *** ***    *** ***        *** ***         ");
+		System.out.println("   ***        ***    ***  ***   *** ************** ************");
+		System.out.println("    ***      ***     ***   ***  *** ************** ************");
+		System.out.println("     ***    ***      ***    *** *** ***        ***          ***");
+		System.out.println("      ***  ***       ***     ****** ***        *** ************");
+		System.out.println("       ******        ***      ***** ***        *** ************");
+		
+		System.out.println("==================================================================");
+		
+		
+		
+	}
+
+}
