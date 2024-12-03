1d764b599ce6afa39f509fb90d613426355c6aed
RealBeboy <93521745+RealBeboy@users.noreply.github.com>
2024-12-03 17:54:46 +0800

    Add files via upload

---
+++ Salvador_Activity4.java
@@ -0,0 +1,16 @@
+package semister1;
+import java.util.Scanner;
+public class Salvador_Activity4 {
+
+	public static void main(String[] args) {
+		String letterA = "a";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter letter A");
+		
+		String input = scanner.nextLine();
+		System.out.println(letterA.toLowerCase());
+		
+		
+	}
+
+}
